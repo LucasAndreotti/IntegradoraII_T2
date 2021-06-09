4b4d9b5ce797ce88b410b260d8958560e28e2e44
Lucas Andreotti <lucasandreotti.02@gmail.com>
2021-06-08 23:08:42 -0300

    Finalização do código que gera os CSVs com as médias de cada parte do estudo

--- medias.py
+++ medias.py
@@ -24,32 +24,85 @@ lst_filtros = [
 '_Escolaridade_SC_PGC',
 ]
 
+def mediaDosFiltros(dataset_medias,path,tipo):
 
+    for filtro in lst_filtros:
+        dataset_filtro = pd.read_csv(path+filtro+'.csv',sep=',')
+        dataset_filtro_media = dataset_filtro.mean()
+
+        lst_medias=[]
+        for media in dataset_filtro_media:
+            if tipo==1:
+                lst_medias.append("{:.2%}".format(media))
+            else:
+                lst_medias.append("{:.2f}".format(media))
+        dataset_medias[filtro]=lst_medias
+
+    return dataset_medias
 
 def mediasParte1():
 
     dataset_medias = {'Questao': ['Q1','Q2','Q3','Q4','Q5','Q6']}
     dataset_medias = pd.DataFrame(dataset_medias)
+        
+    path = dir_path_from+'\\filtros_Parte1\\Parte1'
+
+    dataset_medias = mediaDosFiltros(dataset_medias,path,1)
     #print(dataset_medias)
-
-    for filtro in lst_filtros:
-        dataset_filtro = pd.read_csv(dir_path_from+'\\filtros_Parte1\\Parte1'+filtro+'.csv',sep=',')
-
-        dataset_media = dataset_filtro.mean()
-        
-        lista_medias=[]
-
-        for media in dataset_media:
-            lista_medias.append(media)
-        
-        dataset_medias[filtro] = lista_medias
-    
-    print(dataset_medias)
 
     dataset_medias.to_csv(dir_path_to+'\\medias_Parte1.csv',index=False)
 
-mediasParte1()
+def mediasParte2A():
 
+    dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']}
+    dataset_medias = pd.DataFrame(dataset_medias)
+
+    path = dir_path_from+'\\filtros_Parte2A\\Parte2A'
+
+    dataset_medias = mediaDosFiltros(dataset_medias,path,1)
+
+    dataset_medias.to_csv(dir_path_to+'\\medias_Parte2A.csv',index=False)
+
+def mediasParte2B():
+
+    dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']}
+    dataset_medias = pd.DataFrame(dataset_medias)
+
+    path = dir_path_from+'\\filtros_Parte2B\\Parte2B'
+
+    dataset_medias = mediaDosFiltros(dataset_medias,path,1)
+
+    dataset_medias.to_csv(dir_path_to+'\\medias_Parte2B.csv',index=False)
+
+def mediasCarisma():
+
+    dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']}
+    dataset_medias = pd.DataFrame(dataset_medias)
+
+    path = dir_path_from+'\\filtros_Carisma\\Carisma'
+
+    dataset_medias = mediaDosFiltros(dataset_medias,path,2)
+
+    dataset_medias.to_csv(dir_path_to+'\\medias_Carisma.csv',index=False)
+
+def mediasConforto():
+
+    dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']}
+    dataset_medias = pd.DataFrame(dataset_medias)
+
+    path = dir_path_from+'\\filtros_Conforto\\Conforto'
+
+    dataset_medias = mediaDosFiltros(dataset_medias,path,2)
+
+    dataset_medias.to_csv(dir_path_to+'\\medias_Conforto.csv',index=False)
+
+
+
+mediasParte1()
+mediasParte2A()
+mediasParte2B()
+mediasConforto()
+mediasCarisma()
 
 
 
--- prot_media.py
+++ prot_media.py
@@ -5,40 +5,62 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
-dataset = pd.read_csv(dir_path+'\\CSVs_corrigido\\corrigido_semFiltro_Parte1.csv',sep=",")
+dataset = pd.read_csv(dir_path+'\\CSVs_filtros\\filtros_Carisma\\Carisma_SEM_FILTRO.csv',sep=",")
 
 #print(dataset)
 
 dataset_mean = dataset.mean()
 
-print(dataset_mean)
-exit()
+#print(dataset_mean)
+#exit()
 
-data = {'Questao': ['Q1','Q2','Q3','Q4','Q5','Q6']}
-  
-# Convert the dictionary into DataFrame
+data = {'Questao': ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']}
+
+lst_filtros = [
+'_SEM_FILTRO',
+'_Familiar_Sim',
+'_Familiar_Nao',
+'_Fem',
+'_Masc',
+'_Idade_Menor_30',
+'_Idade_Maior_30',
+'_Escolaridade_EMI_EMC_SI',
+'_Escolaridade_SC_PGC',
+]
+
 df = pd.DataFrame(data)
-  
-# Declare a list that is to be converted into a column
-#address = ['Delhi', 'Bangalore', 'Chennai', 'Patna']
-  
-# Using 'Address' as the column name
-# and equating it to the list
-#df['Address'] = address
-  
-# Observe the result
+
+for filtro in lst_filtros:
+    df_filtro = pd.read_csv(dir_path+'\\CSVs_filtros\\filtros_Carisma\\Carisma'+filtro+'.csv',sep=',')
+    df_filtro_media = df_filtro.mean()
+
+    lista = []
+    for media in df_filtro_media:
+        lista.append(media)
+    df[filtro] = lista
+
+print(df)
+
+df.to_csv(dir_path+'\\teste.csv',index=False)
 
 
-lista=[]
 
+""" print("++++++++++++++++++++++++++++\n")
+print(filtro)
+print(df_filtro_media)
+print("\n++++++++++++++++++++++++++++\n") """
+
+
+#lista=[]
+
+""" 
 for i in dataset_mean:
     lista.append(i)
-
+ """
 #print(lista)
 
-df['teste'] = lista
-print(df)
+#df['teste'] = lista
+#print(df)
 
 
 """ 
@@ -54,7 +76,7 @@ maior_30
 emi_emc_si
 sc_pgc
 
-        semFiltro    Fem    Masc    Fam_Sim     Fam_Nao     menor_30    maior_30    emi_emc_si      sc_pg
+Questao        semFiltro    Fem    Masc    Fam_Sim     Fam_Nao     menor_30    maior_30    emi_emc_si      sc_pg
 
         
 Q1
