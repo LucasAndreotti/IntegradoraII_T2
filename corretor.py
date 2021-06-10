15c045f35fa4e72f15a0d0c3db5cecf7c609bd8e
Lucas Andreotti <lucasandreotti.02@gmail.com>
2021-06-10 00:05:11 -0300

    pequenas simplificações no código gerador_csv.py

--- corretor.py
+++ corretor.py
@@ -42,50 +42,6 @@ def correcao(nome):
 
     dataset.to_csv(dir_path_to+'\\corrigido_semFiltro_'+nome+'.csv',index=False)    
 
-""" def correcaoParte1():
-
-    datasetP1 = pd.read_csv(dir_path_from +'\\formatado_semFiltro_Parte1.csv', sep=',')
-
-    lst_QuestoesP1 = ['Q1','Q2','Q3','Q4','Q5','Q6']
-    lst_RespostasP1 = ['Tristeza','Felicidade','Surpresa','Medo','Raiva','Desgosto']
-
-    for i in range(6):
-        questao = lst_QuestoesP1[i]
-        datasetP1[questao] = np.where((datasetP1[questao] != lst_RespostasP1[i]),0,1)
-
-    datasetP1.to_csv(dir_path_to+'\\corrigido_semFiltro_Parte1.csv', index=False)
-
-
-def correcaoParte2A():
-
-    datasetP2A = pd.read_csv(dir_path_from + '\\formatado_semFiltro_Parte2A.csv',sep=',')
-
-    lst_QuestoesP2A = ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']
-    lst_RespostasP2A = ['Felicidade','Surpresa','Tristeza','Medo','Raiva','Medo','Tristeza','Surpresa','Surpresa','Felicidade','Desgosto','Raiva','Medo','Desgosto','Felicidade','Tristeza','Medo','Desgosto','Surpresa','Medo']
-
-    for i in range(20):
-        questao = lst_QuestoesP2A[i]
-        datasetP2A[questao] = np.where((datasetP2A[questao] != lst_RespostasP2A[i]),0,1)
-       
-    datasetP2A.to_csv(dir_path_to+'\\corrigido_semFiltro_Parte2A.csv', index=False)
-
-def correcaoParte2B():
-
-    datasetP2B = pd.read_csv(dir_path_from + '\\formatado_semFiltro_Parte2B.csv',sep=',')
-
-    lst_QuestoesP2B = ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']
-
-    lst_RespostasP2B = ['Tristeza','Tristeza','Felicidade','Raiva','Felicidade','Desgosto','Raiva','Medo','Felicidade','Raiva','Tristeza','Surpresa','Surpresa','Surpresa','Surpresa','Desgosto','Desgosto','Medo','Desgosto','Felicidade']
-
-    for i in range(20):
-        questao = lst_QuestoesP2B[i]
-
-        datasetP2B[questao] = np.where((datasetP2B[questao] != lst_RespostasP2B[i]),0,1)
-
-    datasetP2B.to_csv(dir_path_to+'\\corrigido_semFiltro_Parte2B.csv', index=False)
-
- """ 
-
 lst_partes = ['Parte1','Parte2A','Parte2B']
 
 for parte in lst_partes:
--- gerador_csv.py
+++ gerador_csv.py
@@ -1,6 +1,5 @@
 import collections
 import pandas as pd
-
 import os 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -19,7 +18,7 @@ def padronizador(dataset,tipo):
     colunasParte1 = ['Q1','Q2','Q3','Q4','Q5','Q6']
     colunasParte2 = ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']
 
-    if tipo==1:
+    if tipo=='Parte1':
         dataset.columns = (colunasFixas+colunasParte1)
     else:
         dataset.columns = (colunasFixas+colunasParte2)
@@ -48,10 +47,7 @@ def padronizador(dataset,tipo):
 def carisma():
 
     carisma_dataset = pd.read_csv(dir_path_from+"\\semFiltro_Carisma.csv", sep=';')
-    #print(carisma_dataset)
-    #print((carisma_dataset.columns))
-
-    carisma_dataset = padronizador(carisma_dataset,2)
+    carisma_dataset = padronizador(carisma_dataset,'Carisma')
 
     carisma_dataset = carisma_dataset.replace("Muito carismático", 5)
     carisma_dataset = carisma_dataset.replace("Carismático", 4)
@@ -62,16 +58,10 @@ def carisma():
     carisma_dataset.to_csv(dir_path_to+'\\formatado_semFiltro_Carisma.csv', index=False)
     #print(carisma_dataset)
 
-    #carisma_dataset_mean = carisma_dataset.mean()
-    #carisma_dataset_mean.to_csv('C:/Users/lucas.andreotti/Desktop/03 - Integradora/DadosFormulario/Analise_Conforto_Carisma/python_Carisma/media_formatado_semFiltro_Carisma.csv', index=False)
-
 def conforto():
 
     conforto_dataset = pd.read_csv(dir_path_from+"\\semFiltro_Conforto.csv", sep=';')
-    #print(conforto_dataset)
-    #print((conforto_dataset.columns))
-
-    conforto_dataset = padronizador(conforto_dataset,2)
+    conforto_dataset = padronizador(conforto_dataset,'Conforto')
 
     conforto_dataset = conforto_dataset.replace("Muito confortável", 5)
     conforto_dataset = conforto_dataset.replace("Confortável", 4)
@@ -82,12 +72,9 @@ def conforto():
     conforto_dataset.to_csv(dir_path_to+'\\formatado_semFiltro_Conforto.csv', index=False)
     #print(conforto_dataset)
 
-    #conforto_dataset_mean = conforto_dataset.mean()
-    #conforto_dataset_mean.to_csv('C:/Users/lucas.andreotti/Desktop/03 - Integradora/DadosFormulario/Analise_Conforto_Carisma/python_Carisma/media_formatado_semFiltro_Conforto.csv', index=False)
-
 def acertos_Parte1():
     parte1_dataset = pd.read_csv(dir_path_from+"\\semFiltro_Parte1.csv", sep=';')
-    parte1_dataset = padronizador(parte1_dataset,1)
+    parte1_dataset = padronizador(parte1_dataset,'Parte1')
 
     parte1_dataset = parte1_dataset.replace("Apenas um rosto neutro", "Neutro")
     parte1_dataset = parte1_dataset.replace("Não sei (percebi uma expressão, mas não sei qual)", "Nao_Sei")
@@ -95,15 +82,9 @@ def acertos_Parte1():
 
     parte1_dataset.to_csv(dir_path_to+'\\formatado_semFiltro_Parte1.csv', index=False)
 
-
-
-""" def acertos_Parte2(): """
-
-
-
 def acertos_Parte2A():
     parte2A_dataset = pd.read_csv(dir_path_from+"\\semFiltro_Parte2A.csv", sep=';')
-    parte2A_dataset = padronizador(parte2A_dataset,2)
+    parte2A_dataset = padronizador(parte2A_dataset,'Parte2A')
 
     parte2A_dataset = parte2A_dataset.replace("Nenhuma (não percebi nada a mais)", "Nenhuma")
     parte2A_dataset = parte2A_dataset.replace("Eu não sei (eu percebi alguma coisa mas não sei o que)", "Nao_sei")
@@ -112,7 +93,7 @@ def acertos_Parte2A():
 
 def acertos_Parte2B():
     parte2B_dataset = pd.read_csv(dir_path_from+"\\semFiltro_Parte2B.csv", sep=';')
-    parte2B_dataset = padronizador(parte2B_dataset,2)
+    parte2B_dataset = padronizador(parte2B_dataset,'Parte2B')
 
     parte2B_dataset = parte2B_dataset.replace("Nenhuma (não percebi nada a mais)", "Nenhuma")
     parte2B_dataset = parte2B_dataset.replace("Eu não sei (eu percebi alguma coisa mas não sei o que)", "Nao_sei")
