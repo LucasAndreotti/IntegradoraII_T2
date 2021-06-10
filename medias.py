20baa3962c295925a4f6389f866e80708cd1b8f2
Lucas Andreotti <lucasandreotti.02@gmail.com>
2021-06-10 01:05:34 -0300

    adicionada nos CSVs uma linha media_medias que mostra a media de acertos para todas medias de cada questão

--- medias.py
+++ medias.py
@@ -23,27 +23,41 @@ lst_filtros = [
 '_Escolaridade_SC_PGC',
 ]
 
+media_medias = 0.0
+
 def mediaDosFiltros(nome):
     if nome == 'Parte1':
-        dataset_medias = {'Questao': ['Q1','Q2','Q3','Q4','Q5','Q6']}
+        dataset_medias = {'Questao': ['Q1','Q2','Q3','Q4','Q5','Q6','Media_Medias']}
+        n_questoes = 6
     else:
-        dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26']}
-    
+        dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26','Media_Medias']}
+        n_questoes = 20
+
     dataset_medias = pd.DataFrame(dataset_medias)
     
     path = dir_path_from+'\\filtros_'+nome+'\\'+nome
+
+    global media_medias
 
     for filtro in lst_filtros:
         dataset_filtro = pd.read_csv(path+filtro+'.csv',sep=',')
         dataset_filtro_media = dataset_filtro.mean()
 
+        media_medias = 0.0
         lst_medias=[]
+
         for media in dataset_filtro_media:
-            media = "{:.3f}".format(media)
+            media_medias =  media_medias + media
+            print(media_medias)
+            media = "{:.4f}".format(media)
             media = (media.replace('.',','))
-            #print(type(media))
-            #exit()
             lst_medias.append(media)
+
+        media_medias = media_medias/n_questoes
+        media_medias = "{:.4f}".format(media_medias)
+        media_medias = media_medias.replace('.',',')
+
+        lst_medias.append(media_medias)
         dataset_medias[filtro]=lst_medias
 
     dataset_medias.to_csv(dir_path_to+'\\medias_'+nome+'.csv',index=False,sep=';')
