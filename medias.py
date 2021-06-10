cc57e5fd7de70babd65743ca5e6c4103fc554d7c
Lucas Andreotti <lucasandreotti.02@gmail.com>
2021-06-10 02:56:17 -0300

    (a ser melhorado/continuado)  adição das médias de acertos para questoes quando as expressão são consideradas positivas ou negativas

--- medias.py
+++ medias.py
@@ -1,3 +1,4 @@
+
 import collections
 import pandas as pd
 import numpy as np
@@ -23,51 +24,91 @@ lst_filtros = [
 '_Escolaridade_SC_PGC',
 ]
 
-media_medias = 0.0
+dic = { 'pp':'pos_pos','pn':'pos_neg','nn':'neg_neg','np':'neg_pos'}
+
+dic_alias = {
+7  :	'pn', 
+8  :	'pn' ,
+9  :	'np' ,
+10 :	'nn' ,
+11 :	'np' ,
+12 :	'nn' ,
+13 :	'nn' ,
+14 :	'pn' ,
+15 :	'pp' ,
+16 :	'pn' ,
+17 :    'nn' ,
+18 : 	'np' ,
+19 :	'np' ,
+20 : 	'np' ,
+21 :	'pp' ,
+22 :	'nn' ,
+23 :	'nn' ,
+24 :	'nn' ,
+25 :	'pn' ,
+26 :	'np' }
 
 def mediaDosFiltros(nome):
     if nome == 'Parte1':
         dataset_medias = {'Questao': ['Q1','Q2','Q3','Q4','Q5','Q6','Media_Medias']}
-        n_questoes = 6
     else:
-        dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26','Media_Medias']}
-        n_questoes = 20
+        dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26','Media_Medias','Media_pp','Media_pn','Media_nn','Media_np']}
 
     dataset_medias = pd.DataFrame(dataset_medias)
     
     path = dir_path_from+'\\filtros_'+nome+'\\'+nome
 
-    global media_medias
-
     for filtro in lst_filtros:
         dataset_filtro = pd.read_csv(path+filtro+'.csv',sep=',')
         dataset_filtro_media = dataset_filtro.mean()
 
-        media_medias = 0.0
-        lst_medias=[]
-
-        for media in dataset_filtro_media:
-            media_medias =  media_medias + media
-            print(media_medias)
-            media = "{:.4f}".format(media)
-            media = (media.replace('.',','))
-            lst_medias.append(media)
-
-        media_medias = media_medias/n_questoes
-        media_medias = "{:.4f}".format(media_medias)
-        media_medias = media_medias.replace('.',',')
-
+        lst_medias = dataset_filtro_media.tolist()
+        media_medias = dataset_filtro_media.mean()
         lst_medias.append(media_medias)
+
+        if nome != 'Parte1':
+            n_questao = 7
+            media_pp = []
+            media_pn = []
+            media_nn = []
+            media_np = []
+
+            for i in range(len(lst_medias)-1):
+                print(n_questao)
+                if(dic[dic_alias[n_questao]]=='pos_pos'):
+                    media_pp.append(lst_medias[i])
+                elif(dic[dic_alias[n_questao]]=='pos_neg'):
+                    media_pn.append(lst_medias[i])
+                elif(dic[dic_alias[n_questao]]=='neg_neg'):
+                    media_nn.append(lst_medias[i])
+                else:    
+                    media_np.append(lst_medias[i])
+                n_questao+=1
+
+            media_pp = sum(media_pp)/len(media_pp)
+            media_pn = sum(media_pn)/len(media_pn)
+            media_nn = sum(media_nn)/len(media_nn)
+            media_np = sum(media_np)/len(media_np)
+
+            lst_medias.append(media_pp)
+            lst_medias.append(media_pn)
+            lst_medias.append(media_nn)
+            lst_medias.append(media_np)
+
+        for i in range(len(lst_medias)):
+            lst_medias[i] = ("{:.4f}".format(lst_medias[i]).replace('.',','))
+
         dataset_medias[filtro]=lst_medias
 
     dataset_medias.to_csv(dir_path_to+'\\medias_'+nome+'.csv',index=False,sep=';')
 
-
 ##
 
-lst_partes = ['Parte1','Parte2A','Parte2B','Carisma','Conforto']
+mediaDosFiltros('Parte2A')
+
+""" lst_partes = ['Parte1','Parte2A','Parte2B','Carisma','Conforto']
 for parte in lst_partes:
-    mediaDosFiltros(parte) 
+    mediaDosFiltros(parte)  """
 
 
 
