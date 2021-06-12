859e1a8f32abfa34797e1aaaf9d12c5b9492b03c
Lucas Andreotti <lucasandreotti.02@gmail.com>
2021-06-11 22:59:09 -0300

    Atualização nos nomes das questões nos CSVs de medias

--- medias.py
+++ medias.py
@@ -48,12 +48,101 @@ dic_alias = {
 25 :	'pn' ,
 26 :	'np' }
 
-def mediaDosFiltros(nome):
-    if nome == 'Parte1':
-        dataset_medias = {'Questao': ['Q1','Q2','Q3','Q4','Q5','Q6','Media_Medias']}
-    else:
-        dataset_medias = {'Questao' : ['Q7','Q8','Q9','Q10','Q11','Q12','Q13','Q14','Q15','Q16','Q17','Q18','Q19','Q20','Q21','Q22','Q23','Q24','Q25','Q26','Media_Medias','Media_pp','Media_pn','Media_nn','Media_np']}
+lst_P1_micro_answers =[
+'Q1_Tristeza',
+'Q2_Felicidade',
+'Q3_Surpresa',
+'Q4_Medo',
+'Q5_Raiva',
+'Q6_Desgosto'
+]
 
+lst_P2_macro_answers=[
+'Q7_Macro_Felicidade',
+'Q8_Macro_Surpresa',
+'Q9_Macro_Tristeza',
+'Q10_Macro_Medo'    ,
+'Q11_Macro_Raiva',
+'Q12_Macro_Medo',
+'Q13_Macro_Tristeza',
+'Q14_Macro_Surpresa',
+'Q15_Macro_Surpresa',
+'Q16_Macro_Felicidade',
+'Q17_Macro_Desgosto',
+'Q18_Macro_Raiva',
+'Q19_Macro_Medo',
+'Q20_Macro_Desgosto',
+'Q21_Macro_Felicidade',
+'Q22_Macro_Tristeza',
+'Q23_Macro_Medo',
+'Q24_Macro_Desgosto',
+'Q25_Macro_Surpresa',
+'Q26_Macro_Medo']
+
+lst_P2_micro_answers=[
+'Q7_Micro_Tristeza',
+'Q8_Micro_Tristeza',
+'Q9_Micro_Felicidade',
+'Q10_Micro_Raiva',
+'Q11_Micro_Felicidade',
+'Q12_Micro_Desgosto',
+'Q13_Micro_Raiva',
+'Q14_Micro_Medo',
+'Q15_Micro_Felicidade',
+'Q16_Micro_Raiva',
+'Q17_Micro_Tristeza',
+'Q18_Micro_Surpresa',
+'Q19_Micro_Surpresa',
+'Q20_Micro_Surpresa',
+'Q21_Micro_Surpresa',
+'Q22_Micro_Desgosto',
+'Q23_Micro_Desgosto',
+'Q24_Micro_Medo',
+'Q25_Micro_Desgosto',
+'Q26_Micro_Felicidade'
+]
+
+lst_P2_micro_macro_answers=[
+'Q7_Felicidade_Tristeza',
+'Q8_Surpresa_Tristeza',
+'Q9_Tristeza_Felicidade',
+'Q10_Medo_Raiva',    
+'Q11_Raiva_Felicidade',
+'Q12_Medo_Desgosto',
+'Q13_Tristeza_Raiva',
+'Q14_Surpresa_Medo',
+'Q15_Surpresa_Felicidade',
+'Q16_Felicidade_Raiva',
+'Q17_Desgosto_Tristeza',
+'Q18_Raiva_Surpresa',
+'Q19_Medo_Surpresa',
+'Q20_Desgosto_Surpresa',
+'Q21_Felicidade_Surpresa',
+'Q22_Tristeza_Desgosto',
+'Q23_Medo_Desgosto',
+'Q24_Desgosto_Medo',
+'Q25_Surpresa_Desgosto',
+'Q26_Medo_Felicidade'
+]
+
+
+def mediaDosFiltros(nome):
+    dataset_medias= {}
+    if nome == 'Parte1':
+        dataset_medias = {'Questao': [lst_P1_micro_answers,'Med_Porcent']}
+    else:
+
+        if nome == 'Carisma' or nome == 'Conforto':
+            lista= lst_P2_micro_macro_answers
+        elif nome == 'Parte2A':
+            lista=lst_P2_macro_answers
+        else:   
+            lista=lst_P2_micro_answers
+
+        lista=lista+['Med_Porcent','Med_PosPos','Med_PosNeg','Med_NegNeg','Med_NegPos']
+        dataset_medias = {'Questao': lista}
+
+    #print(dataset_medias['Questao'],'\n\n')
     dataset_medias = pd.DataFrame(dataset_medias)
     
     path = dir_path_from+'\\filtros_'+nome+'\\'+nome
@@ -74,7 +163,7 @@ def mediaDosFiltros(nome):
             media_np = []
 
             for i in range(len(lst_medias)-1):
-                print(n_questao)
+                #print(n_questao)
                 if(dic[dic_alias[n_questao]]=='pos_pos'):
                     media_pp.append(lst_medias[i])
                 elif(dic[dic_alias[n_questao]]=='pos_neg'):
@@ -95,6 +184,7 @@ def mediaDosFiltros(nome):
             lst_medias.append(media_nn)
             lst_medias.append(media_np)
 
+
         for i in range(len(lst_medias)):
             lst_medias[i] = ("{:.4f}".format(lst_medias[i]).replace('.',','))
 
@@ -105,6 +195,9 @@ def mediaDosFiltros(nome):
 ##
 
 mediaDosFiltros('Parte2A')
+mediaDosFiltros('Parte2B')
+mediaDosFiltros('Conforto')
+mediaDosFiltros('Carisma')
 
 """ lst_partes = ['Parte1','Parte2A','Parte2B','Carisma','Conforto']
 for parte in lst_partes:
